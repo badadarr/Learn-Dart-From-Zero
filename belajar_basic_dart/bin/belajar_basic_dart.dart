38474e13c09958aae5c2ae1cd3fa27776764bc73
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2021-11-27 08:24:05 +0700

    Update

--- belajar_basic_dart/bin/belajar_basic_dart.dart
+++ belajar_basic_dart/bin/belajar_basic_dart.dart
@@ -8,7 +8,15 @@ void main() {
   double height = 175.40;
   bool belajar = true;
 
+  var myName = "badarmaulana";
+  final umur = 17;
+  dynamic tinggi = 1.74;
+  tinggi = "Satu Titik Tujuh Puluh Empat";
+
   var alamat = "Depok, Indonesia";
+
+  print('Halo perkenalkan nama saya $myName salam kenal!');
+  print('Saya Sekarang berumur $umur dan saya memiliki tinggi $tinggi meter');
 
   print(
       "Nama Saya $nama. Sedang membuat $title, dan membuka dengan harga $price, dengan tinggi nya $height");
